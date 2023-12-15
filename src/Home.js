b872a0b64e53c78252ca382f65199e8c1e48a95b
Hamza1610 <muhammadhamza162003@gmail.com>
2023-12-14 16:39:16 -0800

    add pffcanvas to home

--- src/Home.js
+++ src/Home.js
@@ -1,19 +1,31 @@
+import Container from 'react-bootstrap/Container';
+import Navbar from 'react-bootstrap/Navbar';
 import axios from 'axios';
 import { useState } from 'react';
+import Button from 'react-bootstrap/Button';
+import Offcanvas from 'react-bootstrap/Offcanvas';
+import Form from 'react-bootstrap/Form';
+import './all.css';
+import Authenticate from './authenticate-page';
 
-import './home.css';
-const Home = () => {  
+const Home = () => { 
+
     const [postImages, setPostImages] = useState([]);    
     const [post, setPost] = useState('');    
     const [error, setError] = useState(false);
 
+    const [show, setShow] = useState(false);
+
+    const handleClose = () => setShow(false);
+    const handleShow = () => setShow(true);
+
     const handleSubmit = async (e) => {
         e.preventDefault();
-        const userName = localStorage.getItem('userName')
-        console.log(userName);
-        if (userName) {
+        const user = localStorage.getItem('user')
+        console.log(user);
+        if (user) {
             const data = new FormData()
-                data.append("user", userName)
+                data.append("user", user)
                 data.append("message", post)
                 for (let i = 0; i < postImages.length; i++) {
                     data.append('images', postImages[i]);
@@ -34,36 +46,86 @@ const Home = () => {
             
 
         } else {
-            setError("User is unknown please registier to get yourself to use the service")
+            setError("User is unknown please Sign-in to use the our feature")
         }
 
     }
 return (
     <div className="home">
         {error && (
-            <div>Error: {error}</div>
+            <div >Error: {error}</div>
         )}
-        <div className='post-field'>
-            <form onSubmit={handleSubmit}>
-                <h2>Welcome to Autopost</h2>
-                <p>Post to your social media platforms with one click</p>
-                <input
-                    type="file" accept='image/*'
-                    placeholder='Upload Images'
-                    multiple
-                    onChange={(e) => setPostImages(e.target.files)}
-                    required
+
+        {/* Navbar component */}
+        <Navbar className="bg-body-tertiary">
+            <Container>
+                <Navbar.Brand href="#home"><h2>AutoPost</h2></Navbar.Brand>
+                <Navbar.Toggle />
+                <Navbar.Collapse className="justify-content-end">
+                <Navbar.Text>
+                    <Button variant="primary" onClick={handleShow}>
+                        Login States
+                    </Button>
+                </Navbar.Text>
+                </Navbar.Collapse>
+            </Container>
+        </Navbar>
+
+        {/* Offcanvas component */}
+        <Offcanvas show={show} onHide={handleClose}>
+            <Offcanvas.Header closeButton>
+            <Offcanvas.Title>Login States</Offcanvas.Title>
+            </Offcanvas.Header>
+            <Offcanvas.Body>
+                <Authenticate/>
+            </Offcanvas.Body>
+        </Offcanvas>
+
+        {/* Form component */}
+        <div className='post-form-div'>
+            <Form>
+                <Form.Text><h2>Welcome to Autopost</h2></Form.Text>
+                <Form.Text text-muted="true">Post to your social media platforms with one click</Form.Text>
+                <Form.Group
+                    className="mb-3"
+                    controlId="formBasicEmail"
+                    onSubmit={handleSubmit}
+                >
+                    <Form.Control
+                        type="file"
+                        placeholder="Choose the file to upload"
+                        accept='image/*'
+                        multiple
+                        onChange={(e) => setPostImages(e.target.files)}
+                        required
                     />
-                <textarea
-                    value={post}
-                    cols="30" rows="10"
-                    placeholder='Post constent'
-                    onChange={(e) => setPost(e.target.value)}
-                    autoComplete='true'
-                    required
-                    ></textarea>
-                <input type="submit" value='Upload messages'/>
-            </form>
+
+                </Form.Group>
+
+                <Form.Group className="mb-3" controlId="formBasicPassword">
+                    <Form.Control
+                        type="text"
+                        value={post}
+                        placeholder="Post text"
+                        onChange={(e) => setPost(e.target.value)}
+                        autoComplete='true'
+                        required
+                    />
+                </Form.Group>
+                
+                <Form.Group className="mb-3" controlId="formBasicCheckbox">
+                    <Form.Check type="checkbox" label="Mark Facebook" />
+                </Form.Group>
+                <Form.Group className="mb-3" controlId="formBasicCheckbox">
+                    <Form.Check type="checkbox" label="Mark LinkedIn" />
+                </Form.Group>
+                <Form.Group className="mb-3" controlId="formBasicCheckbox">
+                    <Form.Check type="checkbox" label="Mark Twitter" />
+                </Form.Group>
+                <Button variant="primary" type="submit">
+                    Post
+                </Button>
+            </Form>
         </div>
     </div>
 )
