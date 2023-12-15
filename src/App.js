0c00598033ec6ff8efa4f83ccf0ff5bfc952eabb
Hamza1610 <muhammadhamza162003@gmail.com>
2023-12-14 16:37:07 -0800

    added langing page component

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import { BrowserRouter as Router, Routes as Switch, Route } from "react-router-dom";
-import './App.css';
+// import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
 import Home from "./Home";
 import SignUp from "./SignUp";
 import SignIn from "./SignIn";
 import Auth from "./authenticate-page";
+import Landing from "./LandingPage";
 
 function App() {
   
@@ -11,10 +13,12 @@ function App() {
     <Router>
       <div className="App">
         <Switch>
+          <Route path="/" exact Component={Landing} />
           <Route path="/api/" exact Component={Home} />
           <Route path="/sign-up" exact Component={SignUp} />
           <Route path="/auth/" exact Component={Auth} />
           <Route path="/sign-in" exact Component={SignIn} />
+
         </Switch>
       </div>
     </Router>
