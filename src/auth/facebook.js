6104a0be3c0248760a96d7148e236d55941be9b5
Hamza1610 <muhammadhamza162003@gmail.com>
2023-12-14 16:41:15 -0800

    added btn color and indicator

--- src/auth/facebook.js
+++ src/auth/facebook.js
@@ -1,24 +1,51 @@
-import { loginWithPopup, logout, isAuthenticated } from 'firebase/auth';
+import React from "react";
+import { signInWithPopup, FacebookAuthProvider, signOut } from 'firebase/auth';
+import {auth} from './firebase-config';
+import { useState  } from "react";
+import Button from "react-bootstrap/Button";
+
 
 const FacebookLogin = () => {
-  
-  const log = loginWithPopup()
+
+  const [User, setUser] = useState(null)
+
+  const onlineIndicator = { width: '20px', height:'20px', borderRadius:'50%', backgroundColor:'green'}
+  const offlineIndicator = { width: '20px', height:'20px', borderRadius:'50%', backgroundColor:'crimson'}
 
   const handleLogIn = async () => {
-    const response = await log('facebook')
-    console.log('log in', response);
+
+      // Sign in using a popup.
+      const provider = new FacebookAuthProvider();
+      const result = await signInWithPopup(auth, provider);
+
+      // The signed-in user info.
+      const user = result.user;
+      setUser(user);
+      // This gives you a Facebook Access Token.
+      const credential = provider.credentialFromResult(auth, result);
+      const token = credential.accessToken;
+
+      localStorage.setItem('user', JSON.stringify(user));
+      console.log('User:', user);
+      console.log('Token:', token);
+
   };
   const handleLogOut = async () => {
-    // Using logout instead
-    const response = await logout()
-    console.log('Log out: ', response);
+    // Logs out the user
+    try {
+      const response = await signOut(auth)
+       console.log('Log out: ', response);
+    } catch (error) {
+      console.log('Logout error msg: ', error);
+    }
   };
 
   return (
-    <div className="social-div facebook-div">
-      <h4 className="status">Status: {isAuthenticated ? "Online" : "Offline"}</h4>
-      <button onClick={handleLogIn}>Login Facebook</button>
-      <button onClick={handleLogOut}>Logout Facebook</button>
+    <div className="social-div twitter-div mb-3">
+      <h2>Facebook</h2>
+      <h5 className="status">Status: {User ? <div style={onlineIndicator}></div> : <div style={offlineIndicator}></div>}</h5>
+      <Button className="m-2" onClick={handleLogIn}>Login </Button>
+      <Button className="m-2" variant="danger" onClick={handleLogOut}>Logout</Button>
     </div>
   );
 };
