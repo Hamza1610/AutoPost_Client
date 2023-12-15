4dcc03812963973c83d1b45eb8e01166b7501878
Hamza1610 <muhammadhamza162003@gmail.com>
2023-12-14 16:41:44 -0800

    add syle and indicator

--- src/auth/linkedin.js
+++ src/auth/linkedin.js
@@ -1,28 +1,54 @@
 import React from "react";
-import { loginWithPopup, logout, isAuthenticated } from 'firebase/auth';
+import { signInWithPopup, TwitterAuthProvider, signOut } from 'firebase/auth';
+import {auth} from './firebase-config';
+import { useState  } from "react";
+import Button from 'react-bootstrap/Button';
 
 
 const LinkedInLogin = () => {
-  
-  const log = loginWithPopup()
+
+  const [User, setUser] = useState(null)
+
+  const onlineIndicator = { width: '20px', height:'20px', borderRadius:'50%', backgroundColor:'green'}
+  const offlineIndicator = { width: '20px', height:'20px', borderRadius:'50%', backgroundColor:'crimson'}
 
   const handleLogIn = async () => {
-    const response = await log('linkedIn')
-    console.log('log in', response);
+    try {
+      // Sign in using a popup.
+      const provider = new TwitterAuthProvider();
+      const result = await signInWithPopup(auth, provider);
+
+      // The signed-in user info.
+      const user = result.user;
+      setUser(user);
+      // This gives you a Facebook Access Token.
+      const credential = provider.credentialFromResult(auth, result);
+      const token = credential.accessToken;
+
+      console.log('User:', user);
+      console.log('Token:', token);
+    } catch (error) {
+      console.log('Login error msg:',error);
+
+    }
+
   };
   const handleLogOut = async () => {
-    // Using logout instead
-    const response = await logout()
-    console.log('Log out: ', response);
+    // logs user out of account
+    try {
+      const response = await signOut(auth)
+      console.log('Log out: ', response);    
+    } catch (error) {
+      console.log(error);
+    }
   };
 
   return (
-    <div className="social-div linkedin-div">
-      <h4 className="status">Status: {isAuthenticated ? "Online" : "Offline"}</h4>
-      <div className="log-buttons">
-        <button onClick={handleLogIn}>Login LinkedIn</button>
-        <button onClick={handleLogOut}>Logout LinkedIn</button>
-      </div>
+    <div className="social-div twitter-div mb-3">
+      <h2>LinkedIn</h2>
+      <h5 className="status">Status: {User ? <div style={onlineIndicator}></div> : <div style={offlineIndicator}></div>}</h5>
+      <Button className="m-2" onClick={handleLogIn}>Login </Button>
+      <Button className="m-2" variant="danger" onClick={handleLogOut}>Logout</Button>
     </div>
   );
 };
--- src/auth/twitter.js
+++ src/auth/twitter.js
@@ -1,26 +1,56 @@
-import React from "react";
-import { loginWithPopup, logout, isAuthenticated } from 'firebase/auth';
-
+import { useState } from "react";
+import { signInWithPopup, TwitterAuthProvider, signOut } from 'firebase/auth';
+import {auth} from './firebase-config';
+import Button from "react-bootstrap/Button";
 
 const TwitterLogin = () => {
 
-  const log = loginWithPopup()
+  const [User, setUser] = useState(null)
+
+  const onlineIndicator = { width: '20px', height:'20px', borderRadius:'50%', backgroundColor:'green'}
+  const offlineIndicator = { width: '20px', height:'20px', borderRadius:'50%', backgroundColor:'crimson'}
 
   const handleLogIn = async () => {
-    const response = await log('twitter')
-    console.log('log in', response);
+
+    // logs user in
+    try {
+      // Sign in using a popup.
+      const provider = new TwitterAuthProvider();
+      const result = await signInWithPopup(auth, provider);
+
+      // The signed-in user info.
+      const user = result.user;
+      setUser(user)
+      // This gives you a Twitter Access Token and Secret.
+      const credential = TwitterAuthProvider.credentialFromResult(result);
+      const token = credential.accessToken;
+      const secret = credential.secret;
+
+      console.log('User:', user);
+      console.log('Token:', token);
+      console.log('Secret:', secret);    
+    } catch (error) {
+      console.log('Login error:', error);
+    }
+
   };
   const handleLogOut = async () => {
-    // Using logout instead
-    const response = await logout()
-    console.log('Log out: ', response);
+    
+    try {
+      // Using logout instead
+      const response = await signOut(auth)
+      console.log('Log out: ', response); 
+    } catch (error) {
+      
+    }
   };
 
   return (
-    <div className="social-div twitter-div">
-      <h4 className="status">Status: {isAuthenticated ? "Online" : "Offline"}</h4>
-      <button onClick={handleLogIn}>Login Twitter</button>
-      <button onClick={handleLogOut}>Logout Twitter</button>
+    <div className="social-div twitter-div mb-3">
+      <h2>Twitter</h2>
+      <h5 className="status">Status: {User ? <div style={onlineIndicator}></div> : <div style={offlineIndicator}></div>}</h5>
+      <Button className="m-2" onClick={handleLogIn}>Login </Button>
+      <Button className="m-2" variant="danger" onClick={handleLogOut}>Logout</Button>
     </div>
   );
 };
