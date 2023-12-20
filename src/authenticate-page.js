fd58237ff5983cd89a986341e9b75002fe96ed31
Hamza1610 <muhammadhamza162003@gmail.com>
2023-12-19 18:27:43 -0800

    import and configure moduels

--- src/authenticate-page.js
+++ src/authenticate-page.js
@@ -2,16 +2,22 @@ import "./authenticate-page.css";
 import FacebookLogin from './auth/facebook';
 import LinkedInLogin from './auth/linkedin';
 import TwitterLogin from './auth/twitter';
+import {LoginButton, LogoutButton,Profile} from './auth/test_auth_lib';
 import Button from "react-bootstrap/Button";
 
 const Authenticate = () => { 
+
     
-    return (
+  return (
         <div>
         {/* Your application code goes here */}
-        <FacebookLogin />
+        {/* <FacebookLogin />
         <LinkedInLogin />
-        <TwitterLogin />
+        <TwitterLogin /> */}
+
+        <Profile />
+        <LoginButton />
+        <LogoutButton />
         <div className="sign-out-div">
           <Button variant="primary" className="w-50 m-2">Sign out</Button>
           <Button variant="primary" className="w-50 m-2">Delete account</Button>
