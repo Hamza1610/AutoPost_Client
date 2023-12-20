960bea0f0bd70e6415e0817cee4cc8b652c4b356
Hamza1610 <muhammadhamza162003@gmail.com>
2023-12-19 18:29:26 -0800

    add testing auth

---
+++ src/auth/test_auth_lib.js
@@ -0,0 +1,41 @@
+import { useAuth0 } from "@auth0/auth0-react";
+
+// log in function
+const LoginButton = () => {
+  const { loginWithRedirect, loginWithPopup } = useAuth0();
+
+  return <button onClick={() => loginWithRedirect()}>Log In</button>;
+};
+
+// Log out function
+const LogoutButton = () => {
+  const { logout } = useAuth0();
+
+  return (
+    <button onClick={() => logout({ logoutParams: { returnTo: window.location.origin + '/api/' } })}>
+      Log Out
+    </button>
+  );
+};
+
+
+const Profile = () => {
+  const { user, isAuthenticated, isLoading } = useAuth0();
+
+  if (isLoading) {
+    return <div>Loading ...</div>;
+  }
+
+  return (
+    isAuthenticated && (
+      <div>
+        <img src={user.picture} alt={user.name} />
+        <h2>{user.name}</h2>
+        <p>{user.email}</p>
+      </div>
+    )
+  );
+};
+
+
+export {LoginButton, LogoutButton,Profile}
